f23a8144c59b358d7c492facdfee31705efe7714
hk-567 <115183205+hk-567@users.noreply.github.com>
2023-05-26 20:25:34 +0100

    Create FileModel.cs

---
+++ classes/FileModel.cs
@@ -0,0 +1,9 @@
+public class FileModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ContentType { get; set; }
+    public int Size { get; set; }
+    public byte[] Data { get; set; }
+}
+
