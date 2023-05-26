df0679e897517d14ed498dd13f5bd3c1e92ec7cc
hk-567 <115183205+hk-567@users.noreply.github.com>
2023-05-26 20:24:06 +0100

    Update PasswordHelper.cs

--- classes/PasswordHelper.cs
+++ classes/PasswordHelper.cs
@@ -3,28 +3,28 @@ using System.Configuration;
 using System.Data.SqlClient;
 using System.Diagnostics;
 
-namespace NEA
+namespace HabibaKhatun_NEA.classes
 {
     public class PasswordHelper
     {
         private static readonly string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString; // from web.config file
+   private static string HashPassword(string password, string salt)
+       {
+            
+return BCrypt.Net.BCrypt.HashPassword(password + salt, BCrypt.Net.BCrypt.GenerateSalt());
+}
 
-        // Hash a password using a salt
-        private static string HashPassword(string password, string salt)
-        {
-            //return BCrypt.Net.BCrypt.HashPassword(password + salt);
-            return BCrypt.Net.BCrypt.HashPassword(password + salt, BCrypt.Net.BCrypt.GenerateSalt());
-        }
-
-        // Verify a password
-        private static bool VerifyPassword(string password, string salt, string hashedPassword)
-        {
-
+// Verify a password
+        private static bool VerifyPassword(string password, string salt, string hashedPassword) {
+if (string.IsNullOrEmpty(password) || string.IsNullOrEmpty(salt) || string.IsNullOrEmpty(hashedPassword))
+    {
+        return false;
+    }
             return BCrypt.Net.BCrypt.Verify(password + salt, hashedPassword); // called in login button event  
         }
 
-        // Store the password in the database
-        public static void StorePassword(string username, string email, string password, string type)
+ // Store the password in the database
+        public static void StorePassword(string username, string email, string password)
         {
             // Generates a new salt for each password
             string salt = Guid.NewGuid().ToString();
@@ -32,14 +32,14 @@ namespace NEA
             // Hashes the password
             string hashedPassword = HashPassword(password, salt);
 
-            //string activationCode = Guid.NewGuid().ToString();
+            string activationCode = Guid.NewGuid().ToString();
 
             // Stores the username, hashed password, and salt in the database
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
 
-                string sql = "INSERT INTO UsersTbl (Username, Email, Password, Salt, Type ) VALUES (@Username, @Email, @Password, @Salt, @Type)";
+                string sql = "INSERT INTO UsersTbl (Username, Email, Password, Salt ) VALUES (@Username, @Email, @Password, @Salt)";
 
                 using (SqlCommand command = new SqlCommand(sql, connection))
                 {
@@ -47,66 +47,56 @@ namespace NEA
                     command.Parameters.AddWithValue("@Email", email);
                     command.Parameters.AddWithValue("@Password", hashedPassword);
                     command.Parameters.AddWithValue("@Salt", salt);
-                    command.Parameters.AddWithValue("Type",type);
-                    //command.Parameters.AddWithValue("@ActivationCode", activationCode);
+                    command.Parameters.AddWithValue("@ActivationCode", activationCode);
 
 
                     command.ExecuteNonQuery();
                 }
-                // Send an email to the user's email address containing a link to activate their account with the activation code
-                string activationLink = $"http://jw&co.com/activate.aspx?code={activationCode}";
-                string subject = "Activate your account";
-                string body = $"Click on the following link to activate your account: {activationLink}";
-                EmailHelper.SendEmail(email, subject, body);
-
-                connection.Close();
+                               connection.Close();
             }
 
         }
-
-
 
         // Verify the password when logging in
-        public static bool VerifyLogin(string username, string password)
+       public static bool VerifyLogin(string username, string password)
+{
+    // Retrieve the hashed password and salt for the username from the database
+
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+
+        string sql = "SELECT Password,Salt FROM UsersTbl WHERE Username = @Username";
+
+        using (SqlCommand command = new SqlCommand(sql, connection))
         {
-            // Retrieve the hashed password and salt for the username from the database
-          
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            command.Parameters.AddWithValue("@Username", username);
+
+            using (SqlDataReader reader = command.ExecuteReader())
             {
-                connection.Open();
-
-                string sql = "SELECT Password, Salt FROM UsersTbl WHERE Username = @Username";
-
-                using (SqlCommand command = new SqlCommand(sql, connection))
+                if (reader.Read())
                 {
-                    command.Parameters.AddWithValue("@Username", username);
-
-                    using (SqlDataReader reader = command.ExecuteReader())
+                    string storedHashedPassword = reader["Password"] as string;
+                    if (storedHashedPassword != null)
                     {
-                        if (reader.Read())
-                        {
-                            string storedHashedPassword = reader["Password"] as string;
-                            if (storedHashedPassword != null)
-                            {
-                                Debug.WriteLine("stored" + reader["Password"]);
+                        Debug.WriteLine("stored" + reader["Password"]);
 
-                                string salt = reader["Salt"] as string;
-                                string hashedPassword = HashPassword(password, salt); // arguments are the user entered password and the salt from db
+                        string salt = reader["Salt"] as string;
+                        string hashedPassword = HashPassword(password, salt); // arguments are the user entered password and the salt from db
 
-                                // Compares the hashed passwords
-                                bool myBool = VerifyPassword(password, salt, storedHashedPassword);
-                                return myBool;
-                            }
-
-                     
-                        }
+                        // Compares the hashed passwords
+                        bool myBool = VerifyPassword(password, salt, storedHashedPassword);
+                        return myBool;
                     }
+
                 }
-
-                return false;
             }
-
         }
-        
+        return false;
+    }
+
+}
+
     }
 }
+
